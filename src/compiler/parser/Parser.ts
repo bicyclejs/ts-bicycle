53063ee69483a678f7b328fb239ae5327bf207c1
Forbes Lindesay <forbes@lindesay.co.uk>
2017-08-09 13:56:38 +0100

    Normalize casing of locations in parser

--- src/compiler/parser/Parser.ts
+++ src/compiler/parser/Parser.ts
@@ -6,7 +6,11 @@ export default class Parser {
   public readonly program: ts.Program;
   public readonly checker: ts.TypeChecker;
   public readonly scalarNames = new Map<ScalarID, ScalarName>();
+  public readonly fileNames = new Map<string, string>();
   constructor(fileNames: string[], options: ts.CompilerOptions) {
+    fileNames.forEach(name => {
+      this.fileNames.set(name.toLowerCase(), name);
+    })
     // Build a program using the set of root file names in fileNames
     this.program = ts.createProgram(fileNames, options);
 
@@ -17,10 +21,11 @@ export default class Parser {
     const source = node.getSourceFile();
     const {line, character} = source.getLineAndCharacterOfPosition(node.pos);
     const lines = source.text.split('\n');
+    const fileName = this.fileNames.get(source.fileName.toLowerCase()) || source.fileName;
     if (character >= lines[line].length) {
-      return {fileName: source.fileName, line: line + 2};
+      return {fileName, line: line + 2};
     } else {
-      return {fileName: source.fileName, line: line + 1};
+      return {fileName, line: line + 1};
     }
   }
   withLoc(type: () => ValueType, node: ts.Node): ValueType {
