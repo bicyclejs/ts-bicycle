2448e4dabbdb3e54c2fbfcaa991ad44cbbab1f3b
Forbes Lindesay <forbes@lindesay.co.uk>
2018-01-25 16:30:46 +0000

    Add workaround for prettier producing different result on second pass

--- src/formatResult.ts
+++ src/formatResult.ts
@@ -6,5 +6,15 @@ export default async function formatResult(
 ): Promise<string> {
   const options = (await prettier.resolveConfig(filename, {})) || {};
   options.parser = 'typescript';
-  return prettier.format(src, options);
+  // run prettier until it stabalises
+  // workaround for https://github.com/prettier/prettier/issues/3823
+  let formatted = '';
+  let next = '';
+  let i = 0;
+  do {
+    formatted = next;
+    next = prettier.format(src, options);
+    i++;
+  } while (formatted !== next && i < 20);
+  return formatted;
 }
